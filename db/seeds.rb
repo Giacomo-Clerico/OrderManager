e75181048a623d7138ab50604e75f69afd309fc1
Giacomo <98350991+Giacomo-Clerico@users.noreply.github.com>
2025-08-29 12:32:01 +0200

    Add director user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email:"director@mutanda.com", password:"password", user_type: "director")
