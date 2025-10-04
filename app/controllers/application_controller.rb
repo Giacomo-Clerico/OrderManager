b10dfabec61a0fbcc8f6f1091a867e6d95bc4367
Giacomo <giacomo.clerico@outlook.it>
2025-10-04 17:54:09 +0200

    all browser accepted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,31 @@
 class ApplicationController < ActionController::Base
-
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
 
-  allow_browser versions: :modern
+
+
+  allow_browser versions: { all: true } do
+    true
+  end
 
   before_action :authenticate_user!, unless: -> { request.path == new_user_session_path }
 
 
 
+
+
+
+
   # Changes to the importmap will invalidate the etag for HTML responses
 
+
+
   stale_when_importmap_changes
+
+
+
+
+
+
 
 
 
@@ -20,19 +35,27 @@ class ApplicationController < ActionController::Base
 
 
 
-  def authenticate_user!
 
+
+
+
+  def authenticate_user!
     unless current_user
+
+
 
       unless
 
+
+
       redirect_to new_user_session_path, alert: "Please log in first."
+
+
 
       end
 
+
+
     end
-
   end
-
 end
-
